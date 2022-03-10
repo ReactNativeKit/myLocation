6ea83fe6345e91c2066c5810f3920b178816f9aa
Abhinav Anand <abhi9vanand@gmail.com>
2022-03-10 15:59:44 +0530

    appjs updated

--- App.js
+++ App.js
@@ -76,65 +76,77 @@ const styles = StyleSheet.create({
 });
 
 {/**
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { SafeAreaView, StatusBar, StyleSheet } from "react-native"
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps"
 import { check, request, PERMISSIONS, RESULTS } from "react-native-permissions"
+import Geolocation from "react-native-geolocation-service" 
 
 const App = () => {
+  const [location, setLocation] = useState(null) 
+
   const handleLocationPermission = async () => {
-    let permissionCheck = '';
-    if (Platform.OS === 'ios') {
-      permissionCheck = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    let permissionCheck = ""
+    if (Platform.OS === "ios") {
+      permissionCheck = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
 
-      if (
-        permissionCheck === RESULTS.BLOCKED ||
-        permissionCheck === RESULTS.DENIED
-      ) {
+      if (permissionCheck === RESULTS.DENIED) {
         const permissionRequest = await request(
-          PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
-        );
+          PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+        )
         permissionRequest === RESULTS.GRANTED
-          ? console.warn('Location permission granted.')
-          : console.warn('location permission denied.');
+          ? console.warn("Location permission granted.")
+          : console.warn("Location perrmission denied.")
       }
     }
 
-    if (Platform.OS === 'android') {
-      permissionCheck = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+    if (Platform.OS === "android") {
+      permissionCheck = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
 
-      if (
-        permissionCheck === RESULTS.BLOCKED ||
-        permissionCheck === RESULTS.DENIED
-      ) {
+      if (permissionCheck === RESULTS.DENIED) {
         const permissionRequest = await request(
-          PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
-        );
+          PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
+        )
         permissionRequest === RESULTS.GRANTED
-          ? console.warn('Location permission granted.')
-          : console.warn('location permission denied.');
+          ? console.warn("Location permission granted.")
+          : console.warn("Location perrmission denied.")
       }
     }
-  };
+  }
 
   useEffect(() => {
     handleLocationPermission()
   }, [])
 
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords
+        setLocation({ latitude, longitude })
+      },
+      error => {
+        console.log(error.code, error.message)
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    )
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" />
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: 57.709127,
-          longitude: 11.934746,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        showsUserLocation={true}
-      />
+      {location && ( 
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+        />
+      )}
     </SafeAreaView>
   )
 }

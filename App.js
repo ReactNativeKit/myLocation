dbfb2c8dfa721baf3348118d939de9816285304b
Abhinav Anand <abhi9vanand@gmail.com>
2022-03-10 15:18:04 +0530

    app updated

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, Button} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {Permission, PERMISSION_TYPE} from './src/AppPermission';
 import MapView, {Polyline, Marker} from "react-native-maps";
 import MapViewDirections from 'react-native-maps-directions';
@@ -74,3 +74,80 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
 });
+
+{/**
+import React, { useEffect } from "react"
+import { SafeAreaView, StatusBar, StyleSheet } from "react-native"
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps"
+import { check, request, PERMISSIONS, RESULTS } from "react-native-permissions" // 👈
+
+const App = () => {
+  const handleLocationPermission = async () => { // 👈
+    let permissionCheck = '';
+    if (Platform.OS === 'ios') {
+      permissionCheck = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+
+      if (
+        permissionCheck === RESULTS.BLOCKED ||
+        permissionCheck === RESULTS.DENIED
+      ) {
+        const permissionRequest = await request(
+          PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
+        );
+        permissionRequest === RESULTS.GRANTED
+          ? console.warn('Location permission granted.')
+          : console.warn('location permission denied.');
+      }
+    }
+
+    if (Platform.OS === 'android') {
+      permissionCheck = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+
+      if (
+        permissionCheck === RESULTS.BLOCKED ||
+        permissionCheck === RESULTS.DENIED
+      ) {
+        const permissionRequest = await request(
+          PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+        );
+        permissionRequest === RESULTS.GRANTED
+          ? console.warn('Location permission granted.')
+          : console.warn('location permission denied.');
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleLocationPermission()
+  }, [])
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 57.709127,
+          longitude: 11.934746,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true} // 👈
+      />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+})
+
+export default App
+*/}
